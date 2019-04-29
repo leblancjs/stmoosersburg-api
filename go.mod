55837b6872f7d01a4044abbc411e11ffa0494bb4
am91060@ens.etsmtl.ca <am91060@ens.etsmtl.ca>
2019-04-29 11:17:57 -0400

    Added mock SQL driver to facilitate testing user.PostgresRepository. Added unit tests for user.PostgresRepository. Added JetBrains project files to Git ignore file.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/leblancjs/stmoosersburg-api
 go 1.12
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.3.3
 	github.com/gorilla/handlers v1.4.0
 	github.com/gorilla/mux v1.7.1
 	github.com/lib/pq v1.1.0
--- user/postgresrepository_test.go
+++ user/postgresrepository_test.go
@@ -1,7 +1,11 @@
 package user
 
 import (
+	"fmt"
+	"strings"
 	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
 
 	"github.com/leblancjs/stmoosersburg-api/db"
 )
@@ -19,6 +23,223 @@ func TestPostgresRepositoryCreation(t *testing.T) {
 		pr := NewPostgresRepository(database).(*postgresRepository)
 
 		if pr.database != database {
+			t.Fail()
+		}
+	})
+}
+
+func TestPostgresRepositoryCreatingUser(t *testing.T) {
+	queryResultColumns := []string{"id"}
+	expectedQuery := fmt.Sprintf(createQueryFormat, mockUserUsername, mockUserEmail, mockUserPassword)
+
+	t.Run("fails when query returns no rows (user ID can't be retrieved)", func(t *testing.T) {
+		database, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		if err != nil {
+			t.Fatalf("failed to open mock database connection (%s)", err)
+		}
+		defer database.Close()
+
+		pr := NewPostgresRepository(&db.Postgres{DB: database})
+
+		mock.ExpectQuery(expectedQuery).
+			WillReturnRows(mock.NewRows(queryResultColumns))
+
+		if _, err := pr.Create(mockUserUsername, mockUserEmail, mockUserPassword); err == nil {
+			t.Fail()
+		}
+	})
+
+	t.Run("fails when query fails", func(t *testing.T) {
+		database, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		if err != nil {
+			t.Fatalf("failed to open mock database connection (%s)", err)
+		}
+		defer database.Close()
+
+		pr := NewPostgresRepository(&db.Postgres{DB: database})
+
+		mock.ExpectQuery(expectedQuery).
+			WillReturnError(fmt.Errorf("an error occurred"))
+
+		if _, err := pr.Create(mockUserUsername, mockUserEmail, mockUserPassword); err == nil {
+			t.Fail()
+		}
+	})
+
+	t.Run("returns the created user with its new ID when all is well", func(t *testing.T) {
+		database, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		if err != nil {
+			t.Fatalf("failed to open mock database connection (%s)", err)
+		}
+		defer database.Close()
+
+		pr := NewPostgresRepository(&db.Postgres{DB: database})
+
+		mock.ExpectQuery(expectedQuery).
+			WillReturnRows(sqlmock.NewRows(queryResultColumns).AddRow(mockUserID))
+
+		user, err := pr.Create(mockUserUsername, mockUserEmail, mockUserPassword)
+		if err != nil {
+			t.FailNow()
+		}
+		if strings.Compare(mockUserID, user.ID) != 0 {
+			t.Fail()
+		}
+		if strings.Compare(mockUserUsername, user.Username) != 0 {
+			t.Fail()
+		}
+		if strings.Compare(mockUserEmail, user.Email) != 0 {
+			t.Fail()
+		}
+		if strings.Compare(mockUserPassword, user.Password) != 0 {
+			t.Fail()
+		}
+	})
+}
+
+func TestPostgresRepositoryGettingUserByID(t *testing.T) {
+	queryResultColumns := []string{"id", "username", "email", "password"}
+	expectedQuery := getByIDQuery
+
+	t.Run("fails when query returns no rows (no user with given ID exists)", func(t *testing.T) {
+		database, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		if err != nil {
+			t.Fatalf("failed to open mock database connection (%s)", err)
+		}
+		defer database.Close()
+
+		pr := NewPostgresRepository(&db.Postgres{DB: database})
+
+		mock.ExpectQuery(expectedQuery).
+			WithArgs(mockUserID).
+			WillReturnRows(mock.NewRows(queryResultColumns))
+
+		if _, err := pr.GetByID(mockUserID); err == nil {
+			t.Fail()
+		}
+	})
+
+	t.Run("fails when query fails", func(t *testing.T) {
+		database, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		if err != nil {
+			t.Fatalf("failed to open mock database connection (%s)", err)
+		}
+		defer database.Close()
+
+		pr := NewPostgresRepository(&db.Postgres{DB: database})
+
+		mock.ExpectQuery(expectedQuery).
+			WillReturnError(fmt.Errorf("an error occurred"))
+
+		if _, err := pr.GetByID(mockUserID); err == nil {
+			t.Fail()
+		}
+	})
+
+	t.Run("returns the user with the given ID when all is well", func(t *testing.T) {
+		database, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		if err != nil {
+			t.Fatalf("failed to open mock database connection (%s)", err)
+		}
+		defer database.Close()
+
+		pr := NewPostgresRepository(&db.Postgres{DB: database})
+
+		mock.ExpectQuery(expectedQuery).
+			WithArgs(mockUserID).
+			WillReturnRows(
+				sqlmock.NewRows(queryResultColumns).
+					AddRow(mockUserID, mockUserUsername, mockUserEmail, mockUserPassword),
+			)
+
+		user, err := pr.GetByID(mockUserID)
+		if err != nil {
+			t.FailNow()
+		}
+		if strings.Compare(mockUserID, user.ID) != 0 {
+			t.Fail()
+		}
+		if strings.Compare(mockUserUsername, user.Username) != 0 {
+			t.Fail()
+		}
+		if strings.Compare(mockUserEmail, user.Email) != 0 {
+			t.Fail()
+		}
+		if strings.Compare(mockUserPassword, user.Password) != 0 {
+			t.Fail()
+		}
+	})
+}
+
+func TestPostgresRepositoryGettingUserByEmail(t *testing.T) {
+	queryResultColumns := []string{"id", "username", "email", "password"}
+	expectedQuery := getByEmailQuery
+
+	t.Run("fails when query returns no rows (no user with given email exists)", func(t *testing.T) {
+		database, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		if err != nil {
+			t.Fatalf("failed to open mock database connection (%s)", err)
+		}
+		defer database.Close()
+
+		pr := NewPostgresRepository(&db.Postgres{DB: database})
+
+		mock.ExpectQuery(expectedQuery).
+			WithArgs(mockUserEmail).
+			WillReturnRows(mock.NewRows(queryResultColumns))
+
+		if _, err := pr.GetByEmail(mockUserEmail); err == nil {
+			t.Fail()
+		}
+	})
+
+	t.Run("fails when query fails", func(t *testing.T) {
+		database, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		if err != nil {
+			t.Fatalf("failed to open mock database connection (%s)", err)
+		}
+		defer database.Close()
+
+		pr := NewPostgresRepository(&db.Postgres{DB: database})
+
+		mock.ExpectQuery(expectedQuery).
+			WillReturnError(fmt.Errorf("an error occurred"))
+
+		if _, err := pr.GetByEmail(mockUserEmail); err == nil {
+			t.Fail()
+		}
+	})
+
+	t.Run("returns the user with the given email when all is well", func(t *testing.T) {
+		database, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		if err != nil {
+			t.Fatalf("failed to open mock database connection (%s)", err)
+		}
+		defer database.Close()
+
+		pr := NewPostgresRepository(&db.Postgres{DB: database})
+
+		mock.ExpectQuery(expectedQuery).
+			WithArgs(mockUserEmail).
+			WillReturnRows(
+				sqlmock.NewRows(queryResultColumns).
+					AddRow(mockUserID, mockUserUsername, mockUserEmail, mockUserPassword),
+			)
+
+		user, err := pr.GetByEmail(mockUserEmail)
+		if err != nil {
+			t.FailNow()
+		}
+		if strings.Compare(mockUserID, user.ID) != 0 {
+			t.Fail()
+		}
+		if strings.Compare(mockUserUsername, user.Username) != 0 {
+			t.Fail()
+		}
+		if strings.Compare(mockUserEmail, user.Email) != 0 {
+			t.Fail()
+		}
+		if strings.Compare(mockUserPassword, user.Password) != 0 {
 			t.Fail()
 		}
 	})
